d5befff85c43b70bb6fbade0e69253beb2228877
Nazmul Hossain <hossainnazmul191@gmail.com>
2023-11-10 19:25:49 +0600

    practice multiple model pass to a component

---
+++ src/components/TextComponent.vue
@@ -0,0 +1,17 @@
+<script setup>
+    const props = defineProps(['fname','lname']);
+    const emit = defineEmits(['update:fname','update:lname'])
+</script>
+
+<template>
+       <h2>child component</h2>
+       <p>
+        <label>Name</label> <br />
+        <input type="text" :value="fname" @input="$emit('update:fname', $event.target.value)"><br/>
+        <label>Email</label> <br />
+        <input type="text" :value="lname" @input="$emit('update:lname', $event.target.value)"><br/>
+      </p>
+</template>
+
+<style scoped>
+</style>
