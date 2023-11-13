d9cbee4e191e12996eaddec7e771374650f166d8
nazmul <hossainnazmul191@gmail.com>
2023-11-13 12:34:44 +0600

    slot basic uses

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-    import ComponentA from './components/ComponentA.vue';
-    import {person} from './store/data.js';
+   import Button from './components/Button.vue'
 
  </script>
 
@@ -8,16 +7,15 @@
       <div class="container mx-auto">
         <div class="flex items-center mt-5">
           <img class="logo" src="./assets/logo.svg" alt="Vue logo" />
-          <h1 class="text-2xl">Component Model</h1>
+          <h1 class="text-2xl">Component Slot</h1>
         </div>
 
         <div class="mt-10">
-          Original Name: {{ person.name }}<br/>
+             <Button>
+                <b>Add Category</b>
+              </Button>
         </div>
 
-        <div class="mt-10">
-            <ComponentA />
-        </div>
 
       </div>
 </template>
---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<script setup>
+// const props = defineProps(['label']);
+</script>
+ 
+<template>
+    <button class="bg-gray-500 hover:bg-gray-700 text-white py-2 px-4 rounded">
+        <div class="flex items-center" >
+            <span>
+                <slot>
+                    Click Me
+                </slot>
+            </span>
+        </div>
+    </button>
+</template>
+ 
+<style scoped></style>
